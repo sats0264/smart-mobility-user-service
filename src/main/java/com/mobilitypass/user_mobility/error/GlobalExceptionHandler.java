9a4f0552a06a0741a34b2f0d9e9e31272cfe1321
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-09 22:36:08 +0000

    Add NoResourceFoundException handler to GlobalExceptionHandler for improved error handling

--- src/main/java/com/mobilitypass/user_mobility/error/GlobalExceptionHandler.java
+++ src/main/java/com/mobilitypass/user_mobility/error/GlobalExceptionHandler.java
@@ -7,18 +7,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     // Gère spécifiquement l'erreur "Non Trouvé"
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+            WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(
                 LocalDateTime.now(),
                 exception.getMessage(),
                 webRequest.getDescription(false),
-                HttpStatus.NOT_FOUND.value()
-        );
+                HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorDetails> handleNoResource(NoResourceFoundException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                "Resource not found: " + exception.getResourcePath(),
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND.value());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
@@ -29,8 +40,7 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now(),
                 "Une erreur serveur est survenue",
                 webRequest.getDescription(false),
-                HttpStatus.INTERNAL_SERVER_ERROR.value()
-        );
+                HttpStatus.INTERNAL_SERVER_ERROR.value());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
